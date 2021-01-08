d0028f4b40321c5699ca6e7dceaa9c78748270fc
Billy Ceskavich <billy.ceskavich@shopify.com>
2021-01-08 08:04:57 -0500

    Solution: 01

--- 01/main.rb
+++ 01/main.rb
@@ -9,12 +9,12 @@
 require 'pry-byebug'
 
 def format_info(data)
-  puts "Framework: #{data[1]}"
-  puts "Version: #{data[2]}"
-  puts "Workshop: #{data[3]}"
+  puts "Framework: #{data[0]}"
+  puts "Version: #{data[1]}"
+  puts "Workshop: #{data[2]}"
 end
 
 binding.pry # This is our breakpoint, the program will stop executing here
 
-information = ['Ruby', '2.6.2', 'Ruby debugging']
+information = ['Ruby', '2.7.1', 'Ruby debugging']
 format_info(information)
